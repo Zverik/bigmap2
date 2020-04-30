1961408c062574e9d6ed00d2680d2c273123fc35
mfbehrens99 <58851724+mfbehrens99@users.noreply.github.com>
2020-04-30 16:10:04 +0200

    Changed names back

--- www/kml.php
+++ www/kml.php
@@ -1,12 +1,12 @@
 <?php // BigMap 2 KML export. Written by Ilya Zverev, licensed WTFPL.
 header('Content-type: application/vnd.google-earth.kml+xml');
-header('Content-disposition: attachment; filename="' . $name . '.kml"');
+header('Content-disposition: attachment; filename="bigmap_' . $name . '.kml"');
 echo '<?xml version="1.0" encoding="utf-8"?>'."\n";
 ?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
 	<GroundOverlay>
 		<Icon>
-			<href><?=$name ?>.png</href>
+			<href>bigmap_<?=$name ?>.png</href>
 		</Icon>
 		<LatLonBox>
 			<north><?=$lat_max ?></north>
